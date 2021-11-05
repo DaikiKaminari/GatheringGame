f5c940cf00669ffbd37800a878fb29509da0cfb1
CozyDino <nbouteiller@hotmail.fr>
2021-11-05 15:02:31 +0100

    Empêcher que la taille de la fenêtre puisse être modifié à la souris

--- src/main/java/gatheringgame/client/Window.java
+++ src/main/java/gatheringgame/client/Window.java
@@ -8,6 +8,7 @@ public class Window {
         app.setVisible(true);
         app.setSize(Display.WIDTH, Display.HEIGHT);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setResizable(false);
         app.add(d);
         app.pack();
     }
